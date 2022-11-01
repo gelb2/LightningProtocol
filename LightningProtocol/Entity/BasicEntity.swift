010df2f83c459ce192e5cc68bea0a3d0e19118c9
JunYoungJee <rainmaker8492@gmail.com>
2022-11-01 20:30:02 +0900

    entity 추가

--- LightningProtocol/Entity/BasicEntity.swift
+++ LightningProtocol/Entity/BasicEntity.swift
@@ -7,3 +7,121 @@
 
 // TODO: Entity
 import Foundation
+
+struct RandomPeople: Codable {
+    let results: [Result]
+    let info: Info
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let gender: String
+    let name: Name
+    let location: Location
+    let email: String
+    let login: Login
+    let dob, registered: Dob
+    let phone, cell: String
+    let id: ID
+    let picture: Picture
+    let nat: String
+}
+
+// MARK: - Name
+struct Name: Codable {
+    let title, first, last: String
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let street: Street
+    let city, state, country: String
+    let postcode: Postcode
+    let coordinates: Coordinates
+    let timezone: Timezone
+}
+
+// MARK: - Login
+struct Login: Codable {
+    let uuid, username, password, salt: String
+    let md5, sha1, sha256: String
+}
+
+// MARK: - Dob
+struct Dob: Codable {
+    let date: String
+    let age: Int
+}
+
+// MARK: - ID
+struct ID: Codable {
+    let name: String
+    let value: String?
+}
+
+
+
+// MARK: - Picture
+struct Picture: Codable {
+    let large, medium, thumbnail: String
+}
+
+
+// MARK: - Street
+struct Street: Codable {
+    let number: Int
+    let name: String
+}
+
+enum Postcode: Codable {
+    case integer(Int)
+    case string(String)
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let x = try? container.decode(Int.self) {
+            self = .integer(x)
+            return
+        }
+        if let x = try? container.decode(String.self) {
+            self = .string(x)
+            return
+        }
+        throw DecodingError.typeMismatch(Postcode.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for Postcode"))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .integer(let x):
+            try container.encode(x)
+        case .string(let x):
+            try container.encode(x)
+        }
+    }
+}
+
+// MARK: - Coordinates
+struct Coordinates: Codable {
+    let latitude, longitude: String
+}
+
+
+
+// MARK: - Timezone
+struct Timezone: Codable {
+    let offset, timezoneDescription: String
+
+    enum CodingKeys: String, CodingKey {
+        case offset
+        case timezoneDescription = "description"
+    }
+}
+
+
+
+struct Info: Codable {
+    let seed: String
+    let results, page: Int
+    let version: String
+}
